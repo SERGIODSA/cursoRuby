2a96f3c1b9d15f3bb129cf92270c313cf732a2b9
Sergio <sergiodasilvaa@hotmail.com>
2015-10-23 23:16:28 -0430

    Olvide esto

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
 	validates :description, presence: true
 
-	has_attached_file :image
+	has_attached_file :image, style: {medium: "320x240>"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 	attr_accessible :user_id, :description, :image
